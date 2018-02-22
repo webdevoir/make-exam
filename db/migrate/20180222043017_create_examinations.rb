889a44361bf074862a5c474b93f184e195726d39
ngust <nicholasgust@gmail.com>
2018-02-21 20:31:37 -0800

    examination model created

---
+++ db/migrate/20180222043017_create_examinations.rb
@@ -0,0 +1,14 @@
+class CreateExaminations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :examinations do |t|
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :passwordpassword_digest
+      t.text :intro
+      t.text :conclusion
+      t.string :passcode
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222041437) do
+ActiveRecord::Schema.define(version: 20180222043017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "examinations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.string "passwordpassword_digest"
+    t.text "intro"
+    t.text "conclusion"
+    t.string "passcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_examinations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +47,5 @@ ActiveRecord::Schema.define(version: 20180222041437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "examinations", "users"
 end
