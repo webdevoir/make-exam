f4e98b5373c3b08aa1f8c46f8bf6846509d14957
ngust <nicholasgust@gmail.com>
2018-02-22 21:03:49 -0800

    answers model created

---
+++ db/migrate/20180223050059_create_answers.rb
@@ -0,0 +1,13 @@
+class CreateAnswers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :answers do |t|
+      t.references :question, foreign_key: true
+      t.boolean :correct
+      t.string :type
+      t.string :body
+      t.integer :order
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223022007) do
+ActiveRecord::Schema.define(version: 20180223050059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "question_id"
+    t.boolean "correct"
+    t.string "type"
+    t.string "body"
+    t.integer "order"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "examinations", force: :cascade do |t|
     t.bigint "user_id"
@@ -58,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180223022007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "examinations", "users"
   add_foreign_key "questions", "examinations"
 end
