a3a6b989b6cb65c0c282544da6f3fd11d103c63c
ngust <nicholasgust@gmail.com>
2018-03-13 17:52:23 -0700

    added paypal gem and initializer

---
+++ config/initializers/paypal.rb
@@ -0,0 +1,6 @@
+PayPal::Recurring.configure do |config|
+  config.sandbox = true
+  config.username = "nicholasgust-facilitator_api1.gmail.com"
+  config.password = "PWGG8M2CXCL4AQGC"
+  config.signature = "AbVCh9-TcGBdtBlzyTcVD7i65CtwAKBJlwDUEixUWWSLI61Yq-4dczze"
+end
