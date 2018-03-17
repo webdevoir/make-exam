d190df4bd1c9eca7791729fcde0393dfccfe34c7
ngust <nicholasgust@gmail.com>
2018-03-17 13:23:37 -0700

    upgraded? helper method, and ads removed if upgraded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+
+   protected
+
+	  def upgraded?
+	    Subscription.find_by(user_id: current_user.id)
+	  end
+
+	  helper_method :upgraded?
 end
