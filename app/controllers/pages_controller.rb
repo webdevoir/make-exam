a25fc3e8e170dd03c92d78675cb70360fa57cb79
ngust <nicholasgust@gmail.com>
2018-03-23 12:49:44 -0700

    laning logic log change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,15 +5,16 @@ class PagesController < ApplicationController
 
     def landing
     	if current_user
-	    	if current_user.role == "Admin" 
-	    		redirect_to admin_users_path, notice: "Welcome back Admin!"
-	    	elsif current_user.role == "Seeker" 
-	    		@seeker = Seeker.where(user_id: current_user.id).first
-	        	redirect_to seeker_path(@seeker), notice: "Welcome back!"
-	    	elsif current_user.role == "Employer" 
-	    		@employer = Employer.where(user_id: current_user.id).first
-	        	redirect_to employer_path(@employer), notice: "Welcome back!"
-	        end
+	    	# if current_user.role == "Admin" 
+	    	# 	redirect_to admin_users_path, notice: "Welcome back Admin!"
+	    	# elsif current_user.role == "Seeker" 
+	    	# 	@seeker = Seeker.where(user_id: current_user.id).first
+	     #    	redirect_to seeker_path(@seeker), notice: "Welcome back!"
+	    	# elsif current_user.role == "Employer" 
+	    	# 	@employer = Employer.where(user_id: current_user.id).first
+	     #    	redirect_to employer_path(@employer), notice: "Welcome back!"
+	     #    end
+            redirect_to examinations_path
         else
     		render "landing" 
     	end 
