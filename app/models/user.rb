2e52f358300223ea9668f05972397fe3a4bb06f8
ngust <nicholasgust@gmail.com>
2018-03-24 09:22:57 -0700

    got username and email login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :subscriptions
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, authentication_keys: [:login]
 
   validates_presence_of :age
 
@@ -13,6 +14,33 @@ class User < ApplicationRecord
   validates_presence_of :firstname
 
   validates_presence_of :country
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  validate :validate_username
+
+	def validate_username
+	  if User.where(email: username).exists?
+	    errors.add(:username, :invalid)
+	  end
+	end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_hash).first
+      end
+    end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314032231) do
+ActiveRecord::Schema.define(version: 20180324160111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,8 +99,10 @@ ActiveRecord::Schema.define(version: 20180314032231) do
     t.string "lastname"
     t.string "age"
     t.string "country"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "answers", "questions"
