b6e8741cbe132703be74667a01dd684aa3b5b391
ngust <nicholasgust@gmail.com>
2018-08-08 11:57:40 -0700

    added gender to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180808171137) do
+ActiveRecord::Schema.define(version: 20180808185044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -187,6 +187,7 @@ ActiveRecord::Schema.define(version: 20180808171137) do
     t.string "username"
     t.string "role"
     t.string "image"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
